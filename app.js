e9bd75734aeff8b468288d0ced4761f498f75187
jimmyDef <jimmydef@outlook.fr>
2023-12-24 20:09:39 +0100

    test kanap vhost

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const path = require("path");
-
-const productRoutes = require("./routes/product");
+import express, { expressStatic, urlencoded, json } from "express";
+import { join } from "path";
+const vhost = require("vhost");
+import productRoutes from "./routes/product";
 
 const app = express();
 
@@ -18,40 +18,53 @@ app.use((req, res, next) => {
   next();
 });
 
-const buildPath = path.join(__dirname, "build");
-app.use(express.static(buildPath));
+const buildPath = join(__dirname, "build");
+app.use(expressStatic(buildPath));
+
+
+
+const kanapApp = express();
+kanapApp.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "kanap/index.html"));
+});
+kanapApp.use("/images", expressStatic(join(__dirname, "images")));
+
+
+
+
+// Utilisation de vhost pour chaque sous-domaine
+app.use(vhost("kanap.jimmydef.net", kanapApp));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(buildPath, "discover-me/index.html"));
+  res.sendFile(join(buildPath, "discover-me/index.html"));
 });
-app.get("/kanap", (req, res) => {
-  res.sendFile(path.join(buildPath, "kanap/index.html"));
-});
+// app.get("/kanap", (req, res) => {
+//   res.sendFile(join(buildPath, "kanap/index.html"));
+// });
 app.get("/kaza", (req, res) => {
-  res.sendFile(path.join(buildPath, "kaza/build/index.html"));
+  res.sendFile(join(buildPath, "kaza/build/index.html"));
 });
 app.get("/ohmyfood", (req, res) => {
-  res.sendFile(path.join(buildPath, "ohmyfood/index.html"));
+  res.sendFile(join(buildPath, "ohmyfood/index.html"));
 });
 app.get("/booki", (req, res) => {
-  res.sendFile(path.join(buildPath, "booki/index.html"));
+  res.sendFile(join(buildPath, "booki/index.html"));
 });
 app.get("/gameon", (req, res) => {
-  res.sendFile(path.join(buildPath, "gameon/index.html"));
+  res.sendFile(join(buildPath, "gameon/index.html"));
 });
 app.get("/fisheye", (req, res) => {
-  res.sendFile(path.join(buildPath, "fisheye/index.html"));
+  res.sendFile(join(buildPath, "fisheye/index.html"));
 });
 app.get("/lespetitsplat", (req, res) => {
-  res.sendFile(path.join(buildPath, "lespetitsplats/index.html"));
+  res.sendFile(join(buildPath, "lespetitsplats/index.html"));
 });
 
-app.use("/kanap/images", express.static(path.join(__dirname, "images")));
-// app.use(express.static("images"));
+// app.use("/kanap/images", expressStatic(join(__dirname, "images")));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.use("/api/products", productRoutes);
 
-module.exports = app;
+export default app;
