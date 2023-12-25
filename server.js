8b82a16b9b62c25cdb417f4c3e81acc495d41adb
jimmyDef <jimmydef@outlook.fr>
2023-12-25 10:43:40 +0100

    Refactor server.js to set port using app.set()

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import { createServer } from "http";
-import app, { set } from "./app";
+import app  from "./app";
 
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
@@ -13,7 +13,7 @@ const normalizePort = (val) => {
   return false;
 };
 const port = normalizePort(process.env.PORT || "3000");
-set("port", port);
+app.set("port", port);
 
 const errorHandler = (error) => {
   if (error.syscall !== "listen") {
